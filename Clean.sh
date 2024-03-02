c07d501c15d5c84cd111c1c38fe2dbe3373bd019
Yme <yghuang@mails.ccnu.edu.cn>
2024-03-02 20:34:59 +0100

    v1.0: Helpers for fast Glauber

---
+++ Clean.sh
@@ -0,0 +1,4 @@
+echo "Now clean compiled TGlauberMC-3.2 files..."
+rm TGlauberMC-3.2/runglauber_v3.2_C*
+rm TGlauberMC-3.2/runglauber_v3_2_C*
+echo "Done"
---
+++ rootlogon.C
@@ -0,0 +1,3 @@
+{
+  gROOT->LoadMacro("TGlauberMC-3.2/runglauber_v3.2.C+");
+}
